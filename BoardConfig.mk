1de371094f61f34e0f3b00ad7a825a9902ae6ec7
Qais Yousef <qyousef@google.com>
2024-08-12 14:31:07 +0000

    peridot: Enable Lazy RCU
    And sync other RCU paramters.
    
    Bug: 340860065
    Flag: EXEMPT boot time parameters
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: Ia41a45341e988cddcfcf850afd8398a7dc1a91fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,7 @@ BOARD_KERNEL_CMDLINE := \
     mtdoops.fingerprint=peridot:$(LINEAGE_VERSION)
 
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += rcu_nocbs=all rcutree.enable_rcu_lazy=1
 
 BOARD_BOOTCONFIG := \
     androidboot.console=0 \
