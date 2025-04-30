e15781a18037a5dba9b40cb42d0a6c0e65c79fe8
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-04-30 02:04:23 +0200

    peridot: Enable CameraX extensions support
    - Set `ro.camerax.extensions.enabled=true` in both system.prop and odm.prop
    - Add `camerax-vendor-extensions.jar` to system_ext
    - Grant required permission via `camerax-vendor-extensions.xml`
    
    Enables support for enhanced camera features via CameraX Extensions, improving compatibility with modern camera apps.
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -128,6 +128,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.context_hub.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.context_hub.xml
 
+# CameraExtensions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer3-V1-ndk.vendor \
