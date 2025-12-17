42808db79b456536887e1c39504df2ab73dee62f
BLU.rzyn <agusmhrd1k4@gmail.com>
2025-12-17 17:39:13 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,12 +8,12 @@ git clone -b lineage-23.0 https://github.com/droidcore/vendor_xiaomi_peridot_blu
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b lineage-23.0 --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.1 --depth 1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
-git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.0 --depth 1 https://github.com/peridot-dev/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
@@ -53,19 +53,6 @@ git clone https://github.com/droidcore/packages_apps_GameBar.git packages/apps/G
 # FastCharge (fresh clone)
 echo "Cloning FastCharge..."
 rm -rf packages/apps/FastCharge
-
-echo "Fetching QPR1 compat..."
-cd hardware/lineage/compat
-git fetch https://github.com/sm8635-dev/hardware_lineage_compat lineage-23.1
-git reset --hard FETCH_HEAD
-croot
-
-# Picking sepolicy for QPR1 (cherry pick)
-echo "Picking sepolicy fix..."
-cd device/qcom/sepolicy_vndr/sm8650
-git fetch https://github.com/droidcore/device_qcom_sepolicy_vndr qpr1
-git reset --hard FETCH_HEAD
-croot 
 
 # Refresh signing keys
 if [ -d vendor/evolution-priv/keys ]; then
