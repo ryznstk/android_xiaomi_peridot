874beb379b2ea156c6ab038cf76bc228c158134a
ryznstk <agusmhrd1k4@gmail.com>
2026-01-03 11:30:28 +0700

    Lunaris

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -23,12 +23,12 @@ git clone -b lineage-23.0 https://github.com/droidcore/hardware_xiaomi.git hardw
 # MiuiCamera device tree (fresh clone)
 echo "Cloning MiuiCamera device tree..."
 rm -rf device/xiaomi/peridot-miuicamera
-git clone https://github.com/sm8635-dev/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera
+git clone https://github.com/F6-test/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera
 
 # MiuiCamera vendor tree (fresh clone)
 echo "Cloning MiuiCamera vendor tree..."
 rm -rf vendor/xiaomi/peridot-miuicamera
-git clone https://github.com/sm8635-dev/vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera
+git clone https://github.com/F6-test/vendor-xiaomi-peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera
 
 rm -rf packages/apps/XiaomiDolby
 
@@ -47,11 +47,8 @@ echo "Cloning Gamebar tree..."
 rm -rf packages/apps/GameBar
 git clone https://github.com/droidcore/packages_apps_GameBar.git packages/apps/GameBar
 
-rm -rf device/qcom/sepolicy_vndr/sm8650
-git clone https://github.com/ryznstk/android_device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8650
-
 rm -rf system/sepolicy
-git clone https://github.com/ryznstk/android_system_sepolicy.git system/sepolicy
+git clone https://github.com/droidcore/system_sepolicy.git system/sepolicy
 
 # FastCharge (fresh clone)
 echo "Cloning FastCharge..."
